875334077d6002973f2e124cd5a1cbf9b970f99c
jcnghm <jcnghmpsEgmail.com>
2021-09-14 12:28:26 -0700

    first commmit

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const SignIn = () => {
+    return (
+        <div>
+            <h1>Sign in here</h1>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Home, Dashboard, SignIn } from './components';
+import './styles.css';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+
+        <Route exact path='/'>
+          <Home title={'Drones Inventory'} />
+        </Route>
+
+        <Route path='/dashboard'>
+          <Dashboard></Dashboard>
+        </Route>
+
+        <Route path='/signin'>
+          <SignIn></SignIn>
+        </Route>
+
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
